89c2e0fab29d60206ef3611dc07274add86560a7
BLOOMFLARK <50524041+BLOOMFLARK@users.noreply.github.com>
2019-05-13 23:13:01 +0300

    solution

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,44 @@
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    try:
+        f = open(file_path, 'r')
+        return f.read()
+
+    except FileNotFoundError:
+        return None
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(words):
+    counted_words = {}
+    for word in words:
+        if not counted_words.get(word):
+            counted_words[word] = 1
+        else:
+            counted_words[word] += 1
+
+    counted_words_list = list(counted_words.items())
+    sorted_counted_words_list = sorted(counted_words_list, key=lambda x: x[1], reverse=True)
+    if len(text) < 10:
+        return sorted_counted_words_list
+    else:
+        return sorted_counted_words_list[0:10]
+
+
+def print_most_frequent_words(words_data):
+    for word in words_data:
+        print(word[0])
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+    else:
+        sys.exit('no file path')
+    text = load_data(file_path)
+
+    if not text:
+        sys.exit('file not found')
+    else:
+        print_most_frequent_words(get_most_frequent_words(text.split(' ')))
